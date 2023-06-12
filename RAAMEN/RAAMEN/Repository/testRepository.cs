aa8290ff9945a8e46cf19d9d8971a615adf31ea8
unknown <AgentPringles733@gmail.com>
2023-06-12 18:14:56 +0700

    feat: testRepository

---
+++ RAAMEN/RAAMEN/Repository/testRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RAAMEN.Repository
+{
+    public class testRepository
+    {
+        public static void testMethod()
+        {
+
+        }
+    }
+}
